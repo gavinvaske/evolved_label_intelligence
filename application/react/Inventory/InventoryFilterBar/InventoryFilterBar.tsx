4fe01e5f59f64a9184fb5648cf8ca596df67435b
Gavin Vaske <Gavin.Vaske@gmail.com>
2025-03-24 22:50:17 -0600

    converting InventoryFilterBar to use scss modules

--- application/react/Inventory/InventoryFilterBar/InventoryFilterBar.tsx
+++ application/react/Inventory/InventoryFilterBar/InventoryFilterBar.tsx
@@ -1,26 +1,29 @@
-import React from 'react'
-import './InventoryFilterBar.scss'
+
 import inventoryStore from '../../stores/inventoryStore';
 import { observer } from 'mobx-react-lite';
 import { conditionalQuickFilters, textQuickFilters } from './quickFilters';
 import { FilterBar } from '../../_global/FilterBar/FilterBar';
 import { Link } from 'react-router-dom';
+import * as styles from './InventoryFilterBar.module.scss';
+import * as sharedStyles from '@ui/styles/shared.module.scss'
+import clsx from 'clsx';
+import * as flexboxStyles from '@ui/styles/flexbox.module.scss'
 
 const InventoryFilterBar = observer((_) => {
 
   return (
-    <div id='inventory-filter-bar' className="workflow-filter flex-center-left-row full-width card">
-      <div className='tooltip-top btn-wrapper'> 
-        <Link to='/react-ui/forms/material-order' className='btn-create'><i className="fa-regular fa-plus"></i> Order</Link>
-        <span className="tooltiptext">Order material</span>
+    <div className={clsx(flexboxStyles.flexCenterLeftRow, sharedStyles.fullWidth, sharedStyles.card, styles.inventoryFilterBar)}>
+      <div className={clsx(sharedStyles.tooltipTop, styles.btnWrapper)}> 
+        <Link to='/react-ui/forms/material-order' className={sharedStyles.btnCreate}><i className="fa-regular fa-plus"></i> Order</Link>
+        <span className={clsx(sharedStyles.tooltipText)}>Order material</span>
       </div>
-      <div className='tooltip-top btn-wrapper'> 
-        <Link to='/react-ui/forms/material' className='btn-create'><i className="fa-regular fa-plus"></i> Material</Link>
-        <span className="tooltiptext">Create a new material</span>
+      <div className={clsx(sharedStyles.tooltipTop, styles.btnWrapper)}> 
+        <Link to='/react-ui/forms/material' className={sharedStyles.btnCreate}><i className="fa-regular fa-plus"></i> Material</Link>
+        <span className={clsx(sharedStyles.tooltipText)}>Create a new material</span>
       </div>
-      <div className='tooltip-top btn-wrapper'>
-        <Link to='/react-ui/forms/material-length-adjustment' className='btn-create'>Adjustment</Link>
-        <span className="tooltiptext">Adjust footage for material</span>
+      <div className={clsx(sharedStyles.tooltipTop, styles.btnWrapper)}>
+        <Link to='/react-ui/forms/material-length-adjustment' className={sharedStyles.btnCreate}>Adjustment</Link>
+        <span className={clsx(sharedStyles.tooltipText)}>Adjust footage for material</span>
       </div>
 
       <FilterBar
