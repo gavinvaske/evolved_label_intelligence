42eab4bc0340fc9effdaec022dd95112381a379b
Gavin Vaske <Gavin.Vaske@gmail.com>
2025-03-29 19:15:40 -0600

    convert Summary to use scss module

--- application/react/Inventory/Summary/Summary.tsx
+++ application/react/Inventory/Summary/Summary.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import './Summary.scss'
 import { observer } from 'mobx-react-lite';
 import inventoryStore from '../../stores/inventoryStore';
+import * as styles from './Summary.module.scss';
+import clsx from 'clsx';
 
 const Summary = observer(() => {
 
   return (
-    <div className='material-summary-container'>
-      <div className='card col col-one'>
+    <div className={styles.materialSummaryContainer}>
+      <div className={clsx(styles.card, styles.col)}>
         <h1>Inventory</h1>
       </div>
-      <div className='card col col-two'>
+      <div className={clsx(styles.card, styles.col)}>
         <span>Feet On Hand</span>
         <h2 className='total-length-of-material-in-inventory'>{inventoryStore.getArrivedMaterialsLength()}</h2>
       </div>
-      <div className='card col col-three'>
+      <div className={clsx(styles.card, styles.col)}>
         <span>Net Feet</span>
         <h2 className='net-length-of-material-in-inventory'>{inventoryStore.getNetLengthOfMaterialsInInventory()}</h2>
       </div>
-      <div className='card col col-four'>
+      <div className={clsx(styles.card, styles.col)}>
         <span>Feet On Order</span>
         <h2 className='total-length-of-material-ordered'>{inventoryStore.getNotArrivedMaterialsLength()}</h2>
       </div>
