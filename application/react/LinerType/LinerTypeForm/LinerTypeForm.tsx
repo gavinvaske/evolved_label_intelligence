e93a2266f5c438d237edcb5ef7da0ebc97aa8052
Gavin Vaske <Gavin.Vaske@gmail.com>
2025-03-29 22:55:25 -0600

    convert LinerType components to use scss modules

--- application/react/LinerType/LinerTypeForm/LinerTypeForm.tsx
+++ application/react/LinerType/LinerTypeForm/LinerTypeForm.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 import axios, { AxiosError, AxiosResponse } from 'axios';
-import './LinerTypeForm.scss';
 import { useForm } from 'react-hook-form';
 import { useNavigate, useParams } from "react-router-dom";
 import { Input } from '../../_global/FormInputs/Input/Input';
