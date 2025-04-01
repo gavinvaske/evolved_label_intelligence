59d3fc48361c9b5fb65c88bf4b1038434276d48b
Gavin Vaske <Gavin.Vaske@gmail.com>
2025-03-31 22:20:47 -0600

    convert Unauthorized component to use scss modules

--- application/react/_auth/Unauthorized/Unauthorized.tsx
+++ application/react/_auth/Unauthorized/Unauthorized.tsx
@@ -1,17 +1,19 @@
 import { useLocation } from "react-router-dom"
-import './Unauthorized.scss';
+import * as flexboxStyles from '@ui/styles/flexbox.module.scss'
+import * as styles from './Unauthorized.module.scss'
+import clsx from "clsx";
 
 export const Unauthorized = () => {
-    const location = useLocation();
-    const fromUrl = location.state?.from?.pathname;
+  const location = useLocation();
+  const fromUrl = location.state?.from?.pathname;
 
-    return (
-        <div>
-            <h1>Unauthorized</h1>
-            <p>You do not have access to the requested page ("{fromUrl}").</p>
-            <p>To gain access, speak with the website administrator.</p>
-        </div>
-    )
+  return (
+    <div className={clsx(styles.container, flexboxStyles.flexCenterCenterColumn)}>
+      <h1>Unauthorized</h1>
+      <p>You do not have access to the requested page ("<span className={styles.urlText}>{fromUrl}</span>").</p>
+      <p>To gain access, speak with the website administrator.</p>
+    </div>
+  )
 }
 
 export default Unauthorized;
