9ae725f9a437e923f87df04e5c6e09f09c23ad60
Gavin Vaske <Gavin.Vaske@gmail.com>
2025-05-02 19:51:23 -0600

    refactor: redesign ChangePassword form

--- application/react/_auth/ChangePassword/ChangePassword.tsx
+++ application/react/_auth/ChangePassword/ChangePassword.tsx
@@ -1,11 +1,15 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Input } from '../../_global/FormInputs/Input/Input';
 import { FormProvider, useForm } from 'react-hook-form';
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import { useSuccessMessage } from '../../_hooks/useSuccessMessage';
 import { useErrorMessage } from '../../_hooks/useErrorMessage';
 import { useNavigate, useParams } from 'react-router-dom';
-import * as sharedStyles from '@ui/styles/shared.module.scss';
+import { Button } from '../../_global/Button/Button';
+import * as sharedStyles from '@ui/styles/shared.module.scss'
+import * as formStyles from '@ui/styles/form.module.scss'
+import { PasswordIcon } from '../Login/Login';
+import * as styles from './ChangePassword.module.scss'
 
 export const ChangePassword = () => {
   const newPasswordFieldRef = useRef<HTMLInputElement>(null);
@@ -13,6 +17,8 @@ export const ChangePassword = () => {
   const { handleSubmit } = methods;
   const { mongooseId, token } = useParams();
   const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+  const [showRepeatPassword, setShowRepeatPassword] = useState(false);
 
   useEffect(() => {
     newPasswordFieldRef.current?.focus();
@@ -27,32 +33,56 @@ export const ChangePassword = () => {
       .catch((error: AxiosError) => useErrorMessage(error))
   }
 
+  const handlePasswordIconClicked = () => {
+    setShowPassword(!showPassword);
+  }
+
+  const handleRepeatPasswordIconClicked = () => {
+    setShowRepeatPassword(!showRepeatPassword);
+  }
+
   return (
-    <FormProvider {...methods}>
-      <form id='change-password-form' onSubmit={handleSubmit(onSubmit)}>
-        <Input
-          attribute='password'
-          label="New Password"
-          isRequired={true}
-          ref={newPasswordFieldRef}
-          fieldType='password'
-          dataAttributes={
-            { 'data-test': 'password-input' }
-          }
-        />
-        <Input
-          attribute='repeatPassword'
-          label="Re-type Password"
-          isRequired={true}
-          ref={newPasswordFieldRef}
-          fieldType='password'
-          dataAttributes={
-            { 'data-test': 'repeat-password-input' }
-          }
-        />
-        <button className={sharedStyles.submitButton} type='submit' data-test='change-password-btn'>Save Password</button>
-      </form>
-    </FormProvider>
+    <div className={sharedStyles.pageWrapper}>
+      <div className={sharedStyles.card}>
+        <div className={formStyles.formCardHeader}>
+          <h3>Change Password</h3>
+        </div>
+        <div>
+          <FormProvider {...methods}>
+            <form id='change-password-form' className={styles.changePasswordForm} onSubmit={handleSubmit(onSubmit)}>
+              <Input
+                attribute='password'
+                label="New Password"
+                isRequired={true}
+                ref={newPasswordFieldRef}
+                fieldType={showPassword ? 'text' : 'password'}
+                dataAttributes={
+                  { 'data-test': 'password-input' }
+                }
+                RightIcon={<PasswordIcon showPassword={showPassword} onClick={handlePasswordIconClicked} />}
+              />
+              <Input
+                attribute='repeatPassword'
+                label="Re-type Password"
+                isRequired={true}
+                ref={newPasswordFieldRef}
+                fieldType={showRepeatPassword ? 'text' : 'password'}
+                dataAttributes={
+                  { 'data-test': 'repeat-password-input' }
+                }
+                RightIcon={<PasswordIcon showPassword={showRepeatPassword} onClick={handleRepeatPasswordIconClicked} />}
+              />
+              <Button
+                variant="submit"
+                type="submit"
+              >
+                Save Password
+              </Button>
+            </form>
+          </FormProvider>
+        </div>
+      </div>
+    </div>
   )
 }
 
