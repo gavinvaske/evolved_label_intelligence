4a8566f4a12b6d17a4dcb35f5ab6cb2c9244ab23
Gavin Vaske <Gavin.Vaske@gmail.com>
2025-05-02 23:52:52 -0600

    use Button on Admin panel

--- application/react/AdminPanel/AdminPanel.tsx
+++ application/react/AdminPanel/AdminPanel.tsx
@@ -1,13 +1,10 @@
 import axios from 'axios';
 import { useSuccessMessage } from '../_hooks/useSuccessMessage';
 import { useErrorMessage } from '../_hooks/useErrorMessage';
-import { useNavigate } from 'react-router-dom';
-import * as sharedStyles from '@ui/styles/shared.module.scss'
-import * as styles from './AdminPanel.module.scss';
+import { Button } from '../_global/Button/Button';
+import * as sharedStyles from '@ui/styles/shared.module.scss';
 
 export const AdminPanel = () => {
-  const navigate = useNavigate();
-  
   function calculateInventory() {
     axios.get('/materials/recalculate-inventory')
       .then(() => useSuccessMessage('Inventory successfully calculated!'))
@@ -15,10 +12,10 @@ export const AdminPanel = () => {
   }
 
   return (
-    <div className={styles.pageWrapper}>
+    <div className={sharedStyles.pageWrapper}>
       <h2>Admin Panel</h2>
       <div>
-        <button className={sharedStyles.submitButton} onClick={calculateInventory}>Force Inventory Recalculation</button>
+        <Button color='blue' size='large' onClick={calculateInventory}>Force Inventory Recalculation</Button>
       </div>
 
     </div>
