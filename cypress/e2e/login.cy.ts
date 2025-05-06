d96c0e0860fabbe2be975f0bd7c7a03978c6bbcc
Gavin Vaske <Gavin.Vaske@gmail.com>
2025-05-05 22:48:04 -0600

    test: add login e2e tests

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,3 +1,5 @@
+import { TEST_USER } from "support/testData";
+
 describe('Login Cases', () => {
   it('should show the login page with a email and password field, with the email field in focus', () => {
     cy.visit('/react-ui/login');
@@ -19,57 +21,48 @@ describe('Login Cases', () => {
     cy.contains('Welcome to E.L.I')
   })
 
-  // it('Unauthenticated user should be sent to login page when visiting base route', () => {
-  //   cy.logout();
-  //   const baseRoute = '/';
-  //   cy.visit(baseRoute);
+  it('Unauthenticated user should be sent to login page when visiting base route', () => {
+    cy.logout();
+    const baseRoute = '/';
+    cy.visit(baseRoute);
 
-  //   cy.contains('Login');
-  //   cy.location().should(loc => {
-  //     expect(loc.pathname).to.equal('/react-ui/login')
-  //   })
-  // });
+    cy.contains('Login');
+    cy.location().should(loc => {
+      expect(loc.pathname).to.equal('/react-ui/login')
+    })
+  });
 
-  // it('An error message should be shown for invalid login', () => {
-  //   cy.logout();
-  //   cy.invalidLogin();
-  //   const expectedInvalidLoginErrorMessage = 'Invalid email and/or password'
+  it('An error message should be shown for invalid login', () => {
+    cy.logout();
+    cy.invalidLogin();
+    const expectedInvalidLoginErrorMessage = 'Invalid email and/or password'
 
-  //   cy.location().should(loc => {
-  //     expect(loc.pathname).to.equal('/react-ui/login')
-  //   })
-  //   cy.contains(expectedInvalidLoginErrorMessage)
-  // });
+    cy.location().should(loc => {
+      expect(loc.pathname).to.equal('/react-ui/login')
+    })
+    cy.contains(expectedInvalidLoginErrorMessage)
+  });
 
-  // it('User should see a home page upon login', () => {
-  //   cy.logout();
-  //   cy.login();
+  it('If an unauthenticated user attempts to visit a url, they should be redirected to that url after login', () => {
+    cy.logout();
+    const originallyRequestedUrl = '/react-ui/inventory';
+    cy.visit(originallyRequestedUrl);
 
-  //   cy.location().should(loc => {
-  //     expect(loc.pathname).to.equal('/react-ui/profile')
-  //   })
-  // })
+    /* unauthenticated User should have been redirected to login page */
+    cy.location().should(loc => {
+      expect(loc.pathname).to.equal('/react-ui/login')
+    })
 
-  // it('If an unauthenticated user attempts to visit a url, they should be redirected to that url after login', () => {
-  //   cy.logout();
-  //   const originallyRequestedUrl = '/react-ui/inventory';
-  //   cy.visit(originallyRequestedUrl);
+    /* Type in username and password */
+    cy.get('[data-test=username-input]').type(TEST_USER.email)
+    cy.get('[data-test=password-input]').type(TEST_USER.password)
 
-  //   /* unauthenticated User should have been redirected to login page */
-  //   cy.location().should(loc => {
-  //     expect(loc.pathname).to.equal('/react-ui/login')
-  //   })
-
-  //   /* Type in username and password */
-  //   cy.get('[data-test=username-input]').type(Cypress.env('loginUsername'))
-  //   cy.get('[data-test=password-input]').type(Cypress.env('loginPassword'))
-
-  //   /* Click login button */
-  //   cy.get('[data-test=login-btn]').click();
+    /* Click login button */
+    cy.get('[data-test=login-btn]').click();
     
-  //   /* Exo */
-  //   cy.location().should(loc => {
-  //     expect(loc.pathname).to.equal(originallyRequestedUrl)
-  //   })
-  // });
+    /* Exo */
+    cy.location().should(loc => {
+      expect(loc.pathname).to.equal(originallyRequestedUrl)
+    })
+  });
 })
