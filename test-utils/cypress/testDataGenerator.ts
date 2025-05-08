34c9a5c1f2e9fa48659b67a080efe35617943078
Gavin Vaske <Gavin.Vaske@gmail.com>
2025-05-07 22:04:38 -0600

    cleanup: remove unused/unwanted code

--- test-utils/cypress/testDataGenerator.ts
+++ test-utils/cypress/testDataGenerator.ts
@@ -2,13 +2,3 @@ import { mockData } from '../testDataGenerator';
 
 // Re-export the mock data generator
 export const testDataGenerator = mockData;
-
-// Add any Cypress-specific test data generation functions here
-export const generateUniqueTestData = () => {
-  // Add any Cypress-specific logic for generating unique test data
-  // For example, adding timestamps or unique identifiers
-  return {
-    ...mockData,
-    // Add any overrides or additional generators here
-  };
-}; 
