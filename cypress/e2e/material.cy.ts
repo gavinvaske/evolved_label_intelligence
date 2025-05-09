7c674fd2d03106767db2abc5d94b0d9b3a062039
Gavin Vaske <Gavin.Vaske@gmail.com>
2025-05-08 21:05:31 -0600

    test: finish adding Material cypress tests

--- cypress/e2e/material.cy.ts
+++ cypress/e2e/material.cy.ts
@@ -1,7 +1,6 @@
 import { testDataGenerator } from '@test-utils/cypress/testDataGenerator';
 
 describe('Material Management', () => {
-
   const material = testDataGenerator.Material();
   const uppercasedMaterialId = material.materialId.toUpperCase();
 
@@ -11,7 +10,7 @@ describe('Material Management', () => {
     cy.visit('/react-ui/tables/material');
   });
 
-  it.only('should allow creating and viewing a new die', () => {
+  it('should allow creating and viewing a new material', () => {
     // Click the create new button
     cy.get('[data-test=create-icon-button]').click();
 
@@ -61,51 +60,51 @@ describe('Material Management', () => {
     // Verify we're redirected back to the table
     cy.url().should('include', '/react-ui/tables/material');
 
-    // Verify the new delivery method appears in the table
+    // Verify the new material appears in the table
     cy.get('[data-test=material-table]')
       .should('exist') // waits for the table
       .and('be.visible')
       .should('contain', uppercasedMaterialId);
   });
 
-  // it('should allow searching for a delivery method', () => {
-  //   // Search for the delivery method
-  //   cy.get('[data-test=searchbar]').type(material.dieNumber);
+  it('should allow searching for a material', () => {
+    // Search for the material
+    cy.get('[data-test=searchbar]').type(uppercasedMaterialId);
 
-  //   // Wait for the table to be ready and verify search results
-  //   cy.get('[data-test=die-table]').should('exist');
-  //   cy.get('[data-test=table-row]').should('exist');
-  //   cy.get('[data-test=die-table]')
-  //     .should('contain', uppercasedMaterialId);
-  // });
+    // Wait for the table to be ready and verify search results
+    cy.get('[data-test=material-table]').should('exist');
+    cy.get('[data-test=table-row]').should('exist');
+    cy.get('[data-test=material-table]')
+      .should('contain', uppercasedMaterialId);
+  });
 
-  // it('should allow editing an existing die', () => {
-  //   const updatedSerialNumber = material.serialNumber + ' Updated';
+  it('should allow editing an existing material', () => {
+    const updatedName = material.name + ' Updated';
 
-  //   // Find the row with our test delivery method and click the edit button
-  //   cy.get('[data-test=die-table]')
-  //     .contains(uppercasedMaterialId)
-  //     .closest('[data-test=table-row]')  // Get the row containing our text
-  //     .find('[data-test=row-actions]')  // Find the actions container
-  //     .find('[data-test=row-actions-button]')  // Find the button within actions
-  //     .click();
+    // Find the row with our test material and click the edit button
+    cy.get('[data-test=material-table]')
+      .contains(uppercasedMaterialId)
+      .closest('[data-test=table-row]')  // Get the row containing our text
+      .find('[data-test=row-actions]')  // Find the actions container
+      .find('[data-test=row-actions-button]')  // Find the button within actions
+      .click();
 
-  //   // Click the edit button in the dropdown menu
-  //   cy.get('[data-test=row-actions-menu]')
-  //     .find('[data-test=row-action-item]')
-  //     .contains('Edit')
-  //     .click();
+    // Click the edit button in the dropdown menu
+    cy.get('[data-test=row-actions-menu]')
+      .find('[data-test=row-action-item]')
+      .contains('Edit')
+      .click();
 
-  //   // Update the form
-  //   cy.get('[data-test=die-form]').within(() => {
-  //     cy.get('[data-test=input-serialNumber]').clear().type(updatedSerialNumber);
-  //     cy.get('[data-test=submit-button]').click();
-  //   });
+    // Update the form
+    cy.get('[data-test=material-form]').within(() => {
+      cy.get('[data-test=input-name]').clear().type(updatedName);
+      cy.get('[data-test=submit-button]').click();
+    });
 
-  //   // Wait for the table to be ready and verify the update
-  //   cy.get('[data-test=die-table]').should('exist');
-  //   cy.get('[data-test=table-row]').should('exist');
-  //   cy.get('[data-test=die-table]')
-  //     .should('contain', updatedSerialNumber);
-  // });
+    // Wait for the table to be ready and verify the update
+    cy.get('[data-test=material-table]').should('exist');
+    cy.get('[data-test=table-row]').should('exist');
+    cy.get('[data-test=material-table]')
+      .should('contain', updatedName.toUpperCase());
+  });
 });
