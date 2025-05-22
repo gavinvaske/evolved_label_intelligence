ebfecce489af2db48082cd0508902a4ebf941f6b
Gavin Vaske <Gavin.Vaske@gmail.com>
2025-05-21 18:45:39 -0600

    feat: record who/when adhesiveCategories were softDeleted

--- application/api/controllers/adhesiveCategoryController.ts
+++ application/api/controllers/adhesiveCategoryController.ts
@@ -74,7 +74,7 @@ router.get('/search', async (request: Request<{}, {}, {}, SearchQuery>, response
 
 router.delete('/:mongooseId', async (request, response) => {
     try {
-        const deletedAdhesiveCategory = await AdhesiveCategoryModel.deleteById(request.params.mongooseId)
+        const deletedAdhesiveCategory = await AdhesiveCategoryModel.deleteById(request.params.mongooseId, request.user._id)
     
         return response.status(SUCCESS).json(deletedAdhesiveCategory);
     } catch (error) {
