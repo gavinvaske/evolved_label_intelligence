e8fee41bb25d6f623e9c8f8e6be8600bbe37ce54
Gavin Vaske <Gavin.Vaske@gmail.com>
2025-06-01 17:36:20 -0600

    refactor: cleaning up old endpoints

--- application/api/routes.ts
+++ application/api/routes.ts
@@ -5,8 +5,6 @@ import vendorEndpoints from './controllers/vendorController.ts';
 import materialOrderEndpoints from './controllers/materialOrdersController.ts';
 import productEndpoints from './controllers/productController.ts';
 import materialCategoryEndpoints from './controllers/materialCategoryController.ts';
-import filePlanEndpoints from './controllers/filePlanController.ts';
-import packagingEndpoints from './controllers/packagingController.ts';
 import quoteEndpoints from './controllers/quoteController.ts';
 import dieEndpoints from './controllers/dieController.ts';
 import linerTypeEndpoints from './controllers/linerTypeController.ts';
@@ -27,8 +25,6 @@ export const setupApiRoutes = (app: Application) => {
   app.use('/material-orders', materialOrderEndpoints);
   app.use('/products', productEndpoints);
   app.use('/material-categories', materialCategoryEndpoints);
-  app.use('/file-plan', filePlanEndpoints);
-  app.use('/packaging', packagingEndpoints);
   app.use('/quotes', quoteEndpoints);
   app.use('/dies', dieEndpoints);
   app.use('/liner-types', linerTypeEndpoints);
