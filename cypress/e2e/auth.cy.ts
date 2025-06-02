f75c3ba8dc938ee503e8e65f5e7041e2fa509d4b
Gavin Vaske <Gavin.Vaske@gmail.com>
2025-06-01 18:00:16 -0600

    e2e tests: add e2e tests around login/logout/register

---
+++ cypress/e2e/auth.cy.ts
@@ -0,0 +1,72 @@
+import { TEST_USER } from '../support/testData';
+import { mockData } from '../../test-utils/testDataGenerator';
+
+describe('Auth', () => {
+  it('should login', () => {
+    cy.visit('/react-ui/login');
+    cy.get('input[name="email"]').type(TEST_USER.email);
+    cy.get('input[name="password"]').type(TEST_USER.password);
+    cy.get('button[type="submit"]').click();
+  });
+
+  it('should see message if login fails', () => {
+    const invalidEmail = 'invalid@email.com';
+    cy.visit('/react-ui/login');
+    cy.get('input[name="email"]').type(invalidEmail);
+    cy.get('input[name="password"]').type('password');
+    cy.get('button[type="submit"]').click();
+    cy.contains('Invalid email and/or password');
+  });
+
+  it('should logout', () => {
+    cy.login();
+    // verify we're on the profile page
+    cy.url().should('include', '/react-ui/profile');
+    
+    // Click the user profile button to open dropdown
+    cy.get('[class*="userPictureContainer"]').click();
+    
+    // Click the logout button
+    cy.get('button').contains('Log Out').click();
+    
+    // Verify redirect to login page
+    cy.url().should('include', '/react-ui/login');
+  });
+
+  it('should be able to register and then login', () => {
+    const newUser = mockData.User();
+    
+    // Format the date to YYYY-MM-DD
+    const formattedDate = new Date(newUser.birthDate).toISOString().split('T')[0];
+    
+    // Visit registration page
+    cy.visit('/react-ui/register');
+    
+    // Fill out the registration form
+    cy.get('form').within(() => {
+      cy.get('input[name="firstName"]').type(newUser.firstName);
+      cy.get('input[name="lastName"]').type(newUser.lastName);
+      cy.get('input[name="birthDate"]').type(formattedDate);
+      cy.get('input[name="email"]').type(newUser.email);
+      cy.get('input[name="password"]').type(newUser.password);
+      cy.get('input[name="repeatPassword"]').type(newUser.password);
+      cy.get('button[type="submit"]').click();
+    });
+
+    // Verify redirect to login page
+    cy.url().should('include', '/react-ui/login');
+    
+    // Verify success message
+    cy.contains('Registration was successful!');
+    
+    // Login with new credentials
+    cy.get('input[name="email"]').type(newUser.email);
+    cy.get('input[name="password"]').type(newUser.password);
+    cy.get('button[type="submit"]').click();
+    
+    // Verify successful login (by default, the user is not authorized to view any page upon login)
+    cy.url().should('include', '/react-ui/profile');
+    cy.contains('Unauthorized');
+    cy.contains('You do not have access to the requested page');
+  });
+});
